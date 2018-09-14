f54b9724d5db9cd906776369ef16c87e85c2f5df
Zhongxiao Li <liz0g@kw60027.cbrc.kaust.edu.sa>
2018-09-14 13:05:55 +0300

    unsuccessful attempt

--- assignment1/mapreduce/common_reduce.go
+++ assignment1/mapreduce/common_reduce.go
@@ -1,4 +1,9 @@
 package mapreduce
+import(
+	"fmt"
+	"os"
+	"encoding/json"
+)
 
 // doReduce does the job of a reduce worker: it reads the intermediate
 // key/value pairs (produced by the map phase) for this task, sorts the
@@ -31,4 +36,40 @@ func doReduce(
 	// 	enc.Encode(KeyValue{key, reduceF(...)})
 	// }
 	// file.Close()
+	combined_map:=make(map[string] []string)
+	for i:=0;i<nMap;i++{
+		rname:=reduceName(jobName,i,reduceTaskNumber)
+		fh,err:=os.Open(rname)
+		if err!=nil{
+			panic(err)
+		}
+		dec:=json.NewDecoder(fh)
+		var m map[string] string
+		if err:=dec.Decode(&m);err!=nil{
+			panic(err)
+		}
+		for k,v:=range(m){
+			combined_map[k]=append(combined_map[k],v)
+		}
+		fh.Close()
+	}
+
+	result_map:=make(map[string] string)
+	for k,v:=range(combined_map){
+		result_map[k]=reduceF(k,v)
+	}
+	wname:=mergeName(jobName,reduceTaskNumber)
+	fmt.Println(wname)
+	fh,err:=os.OpenFile(wname,os.O_CREATE|os.O_TRUNC|os.O_WRONLY,0644)
+	if err!=nil{
+		panic(err)
+	}
+	enc:=json.NewEncoder(fh)
+	if err:=enc.Encode(result_map);err!=nil{
+		panic(err)
+	}
+	fh.Close()
+
+
+
 }
